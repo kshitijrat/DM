fff40be49cbdd5e2b971f6386565c829832fe9ec
kshitijrat <kshitijratnawat@gmail.com>
2025-08-03 02:32:52 +0530

    uu

--- Backend/server.js
+++ Backend/server.js
@@ -102,6 +102,6 @@ const path = require("path")
 app.use(express.static(path.join(__dirname, "frontend", "dist")));
 // console.log("path is: ",path);
 // For SPA routing
-app.get("*", (req, res) => {
+app.get(/^\/(?!api).*/, (req, res) => {
   res.sendFile(path.join(__dirname, "frontend", "dist", "index.html"));
 });
